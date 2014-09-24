e981323b333c957f88c8fe4b41cdc61df880c574
zigorrom <zigorrom@gmail.com>
2014-09-24 16:35:24 +0200

    FirstCommit

---
+++ FileObject/FileObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileObject
+{
+    public class FileObject:StreamReader,IParameterContainer
+    {
+        private Dictionary<IParameterHeader, IParameterColumn> m_ParameterColumns;
+        private FileReaderConfig m_config;
+        private Dictionary<IParameterHeader, int> m_ParametersContains;
+        public FileObject(string Path):base(Path)
+        {
+
+        }
+        //public FileObject(MemoryStream Stream)
+        //    : base(Stream)
+        //{ }
+        public FileObject(Stream Stream):base(Stream)
+        {
+ 
+        }
+        private void InitializeParameters()
+        {
+            m_ParameterColumns = new Dictionary<IParameterHeader, IParameterColumn>();
+
+        }
+    }
+}
